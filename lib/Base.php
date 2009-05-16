39ab5d9008b3e023748210025748f19cb88fc3f9
eXtreme <jacek.jedrzejewski@gmail.com>
2009-05-16 10:51:11 +0000

    [Opc:trunk] Initial release of Paginator

--- lib/Base.php
+++ lib/Base.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  OPEN POWER LIBS <http://libs.invenzzia.org>
+ *  OPEN POWER LIBS <http://libs.invenzzia.org>  
  *
  * This file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE. It is also available through
