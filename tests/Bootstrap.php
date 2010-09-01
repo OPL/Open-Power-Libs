01db47fd54cab8573b55444217c1d1feaed2e80a
eXtreme <jacek.jedrzejewski@gmail.com>
2010-09-01 12:32:57 +0200

    Fixing Bootstrap for tests

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,15 +3,13 @@
  * The bootstrap file for unit tests.
  *
  * @author Tomasz "Zyx" Jędrzejewski
- * @copyright Copyright (c) 2009 Invenzzia Group
+ * @copyright Copyright (c) 2009-2010 Invenzzia Group
  * @license http://www.invenzzia.org/license/new-bsd New BSD License
  */
 
 $config = parse_ini_file(dirname(__FILE__).'/../paths.ini', true);
-require($config['Opl'].'Opl/Loader.php');
+require($config['libraries']['Opl'].'Opl/Loader.php');
 
 $loader = new Opl_Loader('_');
-$loader->addLibrary('Opl', $config['Opl']);
-$loader->addLibrary('Opt', $config['Opt']);
-$loader->addLibrary('Extra', './');
+$loader->addLibrary('Opl', $config['libraries']['Opl']);
 $loader->register();
